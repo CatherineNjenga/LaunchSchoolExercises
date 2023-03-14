a5b7969fedd1f82c66de35f4c8a8099d51274819
Catherine Njenga <freshiah.catherine@gmail.com>
2023-03-14 19:10:48 +0300

    Completed Easy 1 exercises

--- index.js
+++ index.js
@@ -117,27 +117,113 @@ function isLeapYear(year) {
   // if divisible by 100 not a leap year, if divisible by 100 and divisible by 400 it is a leap year
   if (year % 4 === 0) {
     if (year % 100 === 0) {
-      return false;
+      if (year % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }  
     } else {
       return true;
     }
-  } else if (year % 100 === 0) {
-    if (year % 400 === 0) {
-      return true;
-    } else {
-      return true;
-    }
+  } else {
+    return false;
   }
 }
-console.log(isLeapYear(2016));
-console.log(isLeapYear(2015));
-console.log(isLeapYear(2100));
-console.log(isLeapYear(2400));
-console.log(isLeapYear(240000));
-console.log(isLeapYear(240001));
-console.log(isLeapYear(2000));
-console.log(isLeapYear(1900));
-console.log(isLeapYear(1752));
-console.log(isLeapYear(1));
-console.log(isLeapYear(100));
-console.log(isLeapYear(400));
+
+// console.log(isLeapYear(2016));
+// console.log(isLeapYear(2015));
+// console.log(isLeapYear(2100));
+// console.log(isLeapYear(2400));
+// console.log(isLeapYear(240000));
+// console.log(isLeapYear(240001));
+// console.log(isLeapYear(2000));
+// console.log(isLeapYear(1900));
+// console.log(isLeapYear(1752));
+// console.log(isLeapYear(1700));
+// console.log(isLeapYear(1));
+// console.log(isLeapYear(100));
+// console.log(isLeapYear(400));
+
+function isLeapYearII(year) {
+  if (year <= 1752) {
+    if (year % 4 === 0) {
+      return true
+    } else {
+      return false;
+    }
+  } else {
+    if (year % 4 === 0) {
+      if (year % 100 === 0) {
+        if (year % 400 === 0) {
+          return true;
+        } else {
+          return false;
+        }  
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }  
+}
+
+console.log(isLeapYearII(2016));
+console.log(isLeapYearII(2015));
+console.log(isLeapYearII(2100));
+console.log(isLeapYearII(2400));
+console.log(isLeapYearII(240000));
+console.log(isLeapYearII(240001));
+console.log(isLeapYearII(2000));
+console.log(isLeapYearII(1900));
+console.log(isLeapYearII(1752));
+console.log(isLeapYearII(1700));
+console.log(isLeapYearII(1));
+console.log(isLeapYearII(100));
+console.log(isLeapYearII(400));
+
+function multisum(num) {
+  // Input: number, output: return/console? number, explicit req: sum of numbers/multiples of 3 or 5 between 1 and the input number inclusive
+  // implicit req: the input number is the limit, there are no multiples/sum only unique numbers
+  // data structure: array, algorithm: with an array to store our numbers, loop through the numbers from 1 to the limit of the inputted number
+  // check if the number is a multiple of 3 or 5, check if already in the array
+  // if not add it, sum up the numbers and return the resulting number
+
+  let arr = [], sum = 0;
+
+  for (let i = 1; i <= num; i += 1) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      if (!arr.includes(i)) {
+        sum += i;
+        arr.push(i);  
+      }  
+    }
+  }
+  return sum;
+}
+
+console.log(multisum(3)); // 3
+console.log(multisum(5)); // 8
+console.log(multisum(10));  // 33
+console.log(multisum(1000));  // 234168
+
+function utf16Value(str) {
+  // Input: string, output: number, explicit req: sum of the UTF-16 values of each char, 
+  // implicit req: what do we do about the whitespaces/ whitespaces don't have value in JS
+  // data-structure-array, algorithm
+  // loop through the char in the string, determine the utf-16 of the char, add to a variable sum, return the end value of sum
+  let sum = 0;
+
+  for (let i = 0; i < str.length; i += 1) {
+    let chrUtfValue = str.charCodeAt(i);
+    sum += chrUtfValue;
+  }
+
+  return sum;
+}
+
+console.log(utf16Value('Four score'));
+console.log(utf16Value('Launch School'));
+console.log(utf16Value('a'));
+console.log(utf16Value(''));
+console.log(utf16Value(' '));
